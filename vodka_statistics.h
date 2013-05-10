44531bf322c05c5dd18fef2a131135f8d0855bb7
Vodkacannon <armend.veseli@yahoo.com>
2013-05-09 22:21:29 -0300

    Create vodka_statistics.h
    Main header file.

---
+++ vodka_statistics.h
@@ -0,0 +1,191 @@
+namespace vodka_stat{
+
+  int intElementCount = 0;
+
+}
+
+double vodka_sum(const std::string& strSet){
+
+	using namespace std;
+
+	double dblNum;
+	
+	string strNum;
+
+	int intLen = strSet.length();
+
+	double sum = 0;
+
+	for(int index = 0; index < intLen; index++){ //loop thru. string
+
+			if(strSet.at(index) == '-'){
+
+				strNum += strSet.at(index);
+
+			}if(strSet.at(index) == '.'){
+			
+				strNum += strSet.at(index);
+
+			}if(isdigit(strSet.at(index))){
+
+				strNum += strSet.at(index);
+
+			}
+
+
+			if(!isdigit(strSet.at(index)) && strSet.at(index) != '.' && strSet.at(index) != '-'){
+		
+				dblNum = atof( strNum.c_str() );
+
+				strNum = "";
+
+				sum += dblNum;
+
+			}
+
+		}
+
+	return sum;
+
+}
+
+double vodka_avg(const std::string& strSet){
+
+	using namespace std;
+
+	double dblNum;
+	
+	string strNum;
+
+	int intLen = strSet.length(), intElementCount = 0;
+
+	double sum = 0;
+
+	for(int index = 0; index < intLen; index++){ //loop thru. string
+
+			if(strSet.at(index) == '-'){
+
+				strNum += strSet.at(index);
+
+			}if(strSet.at(index) == '.'){
+			
+				strNum += strSet.at(index);
+
+			}if(isdigit(strSet.at(index))){
+
+				strNum += strSet.at(index);
+
+
+
+			}
+
+
+			if(index <= intLen - 1){
+
+				if(!isdigit(strSet.at(index)) && strSet.at(index) != '.' && strSet.at(index) != '-'){
+		
+					dblNum = atof(strNum.c_str());
+
+					strNum = "";
+
+					intElementCount++;
+
+					sum += dblNum;
+
+				}
+
+			}
+
+		}
+
+	double avg = sum / intElementCount;
+
+	return avg;
+
+}
+
+double vodka_population_stdev(const std::string& strSet){
+
+	using namespace std;
+
+	double dblNum, sum = 0, avg, diff_from_mean_sum = 0;
+	
+	string strNum;
+
+	int intLen = strSet.length(), intElementCount=0;
+
+	for(int index = 0; index < intLen; index++){ //loop thru. string
+
+			if(strSet.at(index) == '-'){
+
+				strNum += strSet.at(index);
+
+			}if(strSet.at(index) == '.'){
+			
+				strNum += strSet.at(index);
+
+			}if(isdigit(strSet.at(index))){
+
+				strNum += strSet.at(index);
+
+			}
+
+			if(index <= intLen - 1){
+
+				if(!isdigit(strSet.at(index)) && strSet.at(index) != '.' && strSet.at(index) != '-'){
+		
+					dblNum = atof(strNum.c_str());
+
+					strNum = "";
+
+					intElementCount++;
+
+					sum += dblNum;
+
+				}
+
+			}
+
+		}
+
+	avg = sum / intElementCount;
+
+	//Start standard deviation
+
+	for(int index = 0; index < intLen; index++){ //loop thru. string
+
+			if(strSet.at(index) == '-'){
+
+				strNum += strSet.at(index);
+
+			}if(strSet.at(index) == '.'){
+			
+				strNum += strSet.at(index);
+
+			}if(isdigit(strSet.at(index))){
+
+				strNum += strSet.at(index);
+				
+			}
+
+			if(index <= intLen - 1){
+
+				if(!isdigit(strSet.at(index)) && strSet.at(index) != '.' && strSet.at(index) != '-'){
+		
+					dblNum = atof(strNum.c_str());
+
+					strNum = "";
+
+					diff_from_mean_sum += (dblNum - avg) * (dblNum - avg);
+
+				}
+
+			}
+
+		}
+
+	//End standard deviation
+
+	return sqrt(diff_from_mean_sum / intElementCount);
+
+}
